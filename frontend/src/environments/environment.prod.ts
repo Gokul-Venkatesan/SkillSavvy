2b8e687fffbc544374574ae7d87bc5e6c53e6b46
Gokul <gokul.venkatesan@gmail.com>
2025-04-26 20:27:21 +0530

    env file added

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// Gokul, Env variable, 26-Apr-2025
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-fastapi-app.onrender.com'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+// Gokul, Env variable, 26-Apr-2025
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:8000'
+};
