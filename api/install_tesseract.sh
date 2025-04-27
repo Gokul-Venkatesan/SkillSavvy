42ff99b10990945da958f515aa31160c5e43ae23
Gokul <gokul.venkatesan@gmail.com>
2025-04-27 15:57:46 +0530

    tesseract installation changes

--- api/install_tesseract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Update package list and install Tesseract and required libraries
-apt-get update
-apt-get install -y tesseract-ocr
-
-# Install any additional language packs if needed (optional)
-apt-get install -y tesseract-ocr-eng tesseract-ocr-spa  # English, Spanish example
--- api/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run the Tesseract installation script
-bash ./install_tesseract.sh
-
